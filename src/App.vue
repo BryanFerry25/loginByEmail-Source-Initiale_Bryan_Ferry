8bb2e5fcee54930332fdddf1e759de71a44a8cd3
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-09-16 16:39:24 +0200

    ajout de la fonction sign up et sign in

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,8 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
 
 <script>
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'
-const SUPABASE_KEY = 'YOUR_ANON_KEY'
+const SUPABASE_URL = 'https://wfsrjkfzttgvmxhmvlgj.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indmc3Jqa2Z6dHRndm14aG12bGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzMzcxODcsImV4cCI6MTk3ODkxMzE4N30.gZco8C16mob_K3MOKsNF_3_iS3tag4SP_CvjjseLPKo'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 
@@ -39,14 +39,36 @@ export default {
     //this method allows a new user to sign up the system. Once done, the user receives an email
     //asking for account validation. Once the validation made the user is added to the system
     async register(){
+      try { 
+        const { user, session, error } = await supabase.auth.signUp({ 
+          email: this.email, 
+          password: this.passwd, 
+        }); 
+        if (error) throw error; 
+        document.getElementById('status').innerHTML="Please validate the received email "
+      } catch (error) { 
+        alert(error.error_description || error.message); 
+      }  
+    },
       
     },
     //this method allows the already registred user to log in the system.
     async login(){
        
+      try { 
+        const { user, session, error } = await supabase.auth.signIn({ 
+          email: this.email, 
+          password: this.passwd, 
+        });
+        if (error) throw error; 
+        document.getElementById('status').innerHTML='You are now logged !' 
+      } catch (error) { 
+        alert(error.error_description || error.message); 
+      }  
     }
-  }  
-}
+    }
+   
+
 </script>
 
 <style>
